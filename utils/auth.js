55e2f4cc8ed1d2c17ab7a9b799b544ea334bba28
r-yanagimoto <r-yanagimoto@ei.sanken.osaka-u.ac.jp>
2023-03-22 21:43:12 +0900

    ⚡️ トークンの付与を実装

--- utils/auth.js
+++ utils/auth.js
@@ -4,8 +4,7 @@ const auth = async(req, res, next) => {
     if(req.method === "GET"){
         return next()
     }
-    // const token = await req.headers.authorization.split(" ")[1]
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhlbGxvQG1vbm90ZWluLmNvbSIsImlhdCI6MTY3OTQwMzY5NywiZXhwIjoxNjc5NDg2NDk3fQ.3MUNEPTfrcS8QND_syxptIqKIe21MT5PsNvp18n1Q88"
+    const token = await req.headers.authorization.split(" ")[1]
     if(!token){
         return res.status(400).json({message: "トークンがありません"})
     }
